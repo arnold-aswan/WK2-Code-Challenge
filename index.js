9c18d5c9f1351b3c18328a14b296dc6e4fb36330
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-06-25 21:57:18 +0300

    refactored  addVote function

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 
  window.addEventListener("DOMContentLoaded", () => {
+  initialize()
   hideAnimal()
+  addVote()
 })
 
- function renderAnimal(animal) {
+// creates animal cards and assign them values from the db.json file then renders them on the html page
+function renderAnimal(animal) {
   const ul = document.querySelector('.animals')
 
   let li = document.createElement("li")
   li.className = 'animalList'
-  li.setAttribute('name', 'listItem')
 
   li.innerHTML = `
     <article class="animalCard">
@@ -16,10 +18,10 @@
         <img src="${animal.image}" class="animalImg" />
       </div>
       <div class="animalCardBody">
-        <h3 class="name" >${animal.name}</h3>
-        <p class="votesCount">Votes:<span class='vote'> ${animal.votes}</span></p>
+        <h3 class="name">${animal.name}</h3>
+        <p class="votesCount">Votes: <span class='vote'> ${animal.votes}</span></p>
         <div class="buttons">
-          <button class="btn add" onclick="addVote()">Add vote</button>
+          <button class="btn add">Add vote</button>
           <button class="btn reset" onclick="resetVotes()">Reset votes</button>
         </div>
       </div>
@@ -28,42 +30,39 @@
   ul.appendChild(li)
 }
 
+// fetches data from  the db.json file using a GET request
 function renderAnimals() {
     fetch("http://localhost:3000/characters")
     .then(res => res.json())
-    .then(characters => characters.forEach(animal => renderAnimal(animal)))
-    
+    .then(characters => characters.forEach(animal => renderAnimal(animal))) 
 }
 
- function initialize() {
-  renderAnimals()
-  
-}
-
+// function that toggles the hide css style on the animal images and votes
 function hideAnimal() {
   const ul = document.querySelector('ul.animals')
-
   ul.addEventListener("click", (e)=> {
     if (e.target.className === 'name') {
-      const hideElement = e.target.parentElement.previousElementSibling.firstElementChild
-      hideElement.classList.toggle('hide')
+      const hideImage = e.target.parentElement.previousElementSibling.firstElementChild
+      const hideVotes = e.target.nextElementSibling
+      hideImage.classList.toggle('hide')
+      hideVotes.classList.toggle('hide')
     }
   })
 }
 
-function addVote(){ 
-const addVoteBtns = document.querySelectorAll('.btn.add')
-addVoteBtns.forEach((addVoteBtn) => {
-    addVoteBtn.addEventListener("click", (e)=> {
+// adds the vootes of the animals and increase the votes count per click
+function addVote () {
+  const ul = document.querySelector('ul.animals')
+  ul.addEventListener("click", (e)=> {
+    if(e.target.className === 'btn add') {
       const votesCount = e.target.parentElement.previousElementSibling.firstElementChild
-      console.log(votesCount);
-      const currentVote = parseInt(votesCount.textContent)
-      console.log(currentVote);
-      votesCount.textContent = currentVote + 1
-    })
+      const newVote = parseInt(votesCount.textContent)
+      votesCount.textContent = newVote + 1
+    }
   })
 }
 
+// resets the votes to 0 
 function resetVotes(){
   const resetVotesBtn = document.querySelectorAll('.btn.reset')
   resetVotesBtn.forEach(resetBtn => {
@@ -74,5 +73,10 @@ function resetVotes(){
   })
 }
 
-initialize()
+// calls the renderAnimals function
+function initialize() {
+  renderAnimals()
+}
+
+
 
