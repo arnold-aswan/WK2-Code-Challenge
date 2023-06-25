660f467e98c028609c0349b3cbc4891ad6111b7c
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-06-25 17:10:03 +0300

    added reset and add vote functions

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// see a list of all animal names
-// click animal name to see its details
-  // i.e image | no of votes | 
- // display details of one animal at a time
- // add number of votes for each animal + votes
+
+ window.addEventListener("DOMContentLoaded", () => {
+  hideAnimal()
+})
 
  function renderAnimal(animal) {
   const ul = document.querySelector('.animals')
@@ -18,16 +17,16 @@
       </div>
       <div class="animalCardBody">
         <h3 class="name" >${animal.name}</h3>
-        <p class="votesCount">Votes: ${animal.votes}</p>
+        <p class="votesCount">Votes:<span class='vote'> ${animal.votes}</span></p>
         <div class="buttons">
-          <button class="btn add">Add vote</button>
-          <button class="btn reset">Reset votes</button>
+          <button class="btn add" onclick="addVote()">Add vote</button>
+          <button class="btn reset" onclick="resetVotes()">Reset votes</button>
         </div>
       </div>
     </article>
   `
   ul.appendChild(li)
- }
+}
 
 function renderAnimals() {
     fetch("http://localhost:3000/characters")
@@ -39,27 +38,41 @@ function renderAnimals() {
  function initialize() {
   renderAnimals()
   
- }
+}
 
-// function myFunction() {
-  const parentEl = document.querySelector('ul.animals')
+function hideAnimal() {
+  const ul = document.querySelector('ul.animals')
 
-  parentEl.addEventListener("click", (e)=> {
-    if (e.target.tagName === 'H3') {
-      const H3Element = e.target
-      const liEl = e.target.parentElement.previousElementSibling.firstElementChild
-      liEl.classList.toggle('hide')
+  ul.addEventListener("click", (e)=> {
+    if (e.target.className === 'name') {
+      const hideElement = e.target.parentElement.previousElementSibling.firstElementChild
+      hideElement.classList.toggle('hide')
     }
   })
-//  }
-
-function addVote() {
-
 }
 
-function resetVotes() {
-
+function addVote(){ 
+const addVoteBtns = document.querySelectorAll('.btn.add')
+addVoteBtns.forEach((addVoteBtn) => {
+    addVoteBtn.addEventListener("click", (e)=> {
+      const votesCount = e.target.parentElement.previousElementSibling.firstElementChild
+      console.log(votesCount);
+      const currentVote = parseInt(votesCount.textContent)
+      console.log(currentVote);
+      votesCount.textContent = currentVote + 1
+    })
+  })
 }
 
- initialize()
+function resetVotes(){
+  const resetVotesBtn = document.querySelectorAll('.btn.reset')
+  resetVotesBtn.forEach(resetBtn => {
+    resetBtn.addEventListener("click", (e)=> {
+      const votes = e.target.parentElement.previousElementSibling.firstElementChild
+      votes.textContent = 0
+    })
+  })
+}
+
+initialize()
 
